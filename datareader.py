8cf5642873e9d72c3fe5471d7504e52fbafb0384
Ryan Brower <ryan.brower28@gmail.com>
2022-06-17 17:44:24 -0700

    Added search by state

--- datareader.py
+++ datareader.py
@@ -1,100 +1,88 @@
-from hashlib import new
-from itertools import count
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class DataReader:
+
 
     def __init__(self):
         self.db_name = "realtor-data.csv"
         self.test_db = "test-data.csv"
         self.data = pd.read_csv(self.db_name).drop_duplicates(subset=None, keep='first', inplace=False)
+        self.all_states = pd.unique(self.data["state"])
 
-        # self.test()
-        # self.read_data()
+    def add_commas(self, number):
+            return ("{:,}".format(number))
 
-
-    def test(self):
-
-        print(pd.unique(self.data["state"]))
-
-        data = self.data.nlargest(n=40, columns=["price"])
-        print(data.to_string())
-
-
-    def read_data(self):
-
-        data = pd.read_csv(self.db_name)
-
-        # print(data.to_string())
-
-        plt.plot(data['acre_lot'], data['price'], label='price')
-
-
-        # x-coordinates of left sides of bars 
-        left = range(len(data))
-
-        # heights of bars
-        height = data['price']
-
-        states = pd.unique(data['state'])
-
-        # labels for bars
-        tick_label = states
-
-        # plotting a bar chart
-        plt.bar(left, height, tick_label = tick_label,
-                width = 0.8, color = ['green'])
-
-        # naming the x-axis
-        plt.xlabel('x - axis')
-        # naming the y-axis
-        plt.ylabel('y - axis')
-        # plot title
-        plt.title('My bar chart!')
-
-        # function to show the plot
-        plt.show()
+    
+    def get_all_states(self):
+        return self.all_states
 
 
     def get_homes(self, end, quant):
 
         if end == "high":
+
             new_df = self.data.nlargest(n=quant, columns=["price"])
+            new_df["price"] = new_df["price"].apply(self.add_commas)
+            new_df["house_size"] = new_df["house_size"].apply(self.add_commas)
+
             print(new_df[["price", "bed", "bath", 
-                        "acre_lot", "city", "state", 
-                        "house_size"]].to_string())
-
-            print()
-
-            states_list = pd.unique(new_df["state"])
-
-            total_listings = 0
-
-            for state in states_list:
-                state_data = new_df["state"] == state
-                instances = str(state_data).count("True")
-                total_listings += instances
-                print(f"Listings in {state}: {instances}")
-                
-            print(f"Total listings: {total_listings}")
+                        "acre_lot", "full_address", 
+                        "house_size"]].to_string(index=False))
 
         elif end == "low":
+
             new_df = self.data.nsmallest(n=quant, columns=["price"])
-            print(new_df[["price", "acre_lot", "city", 
-                        "state", "house_size"]].to_string())
+            new_df["price"] = new_df["price"].apply(self.add_commas)
+            new_df["house_size"] = new_df["house_size"].apply(self.add_commas)
+
+            print(new_df[["price", "acre_lot", "full_address",
+                        "house_size"]].to_string(index=False))
+
+        print()
+
+        states_list = pd.unique(new_df["state"])
+
+        total_listings = 0
+
+        for state in states_list:
+            state_data = new_df["state"] == state
+            instances = str(state_data).count("True")
+            total_listings += instances
+            print(f"Listings in {state}: {instances}")
             
-            print()
+        print(f"Total listings: {total_listings}")
 
-            states_list = pd.unique(new_df["state"])
 
-            total_listings = 0
+    def get_by_state(self, state):
+        
+        state_df = self.data[self.data["state"].isin([state])]
 
-            for state in states_list:
-                f = new_df["state"] == state
-                p = str(f).count("True")
-                total_listings += p
-                print(f"Listings in {state}: {p}")
-                
-            print(f"Total listings: {total_listings}")
+        print(f"\n      State -- {state}\n")
+
+        print("    Most expensive homes:\n")
+
+        highest_homes = state_df.nlargest(n=10, columns=["price"])
+        highest_homes["price"] = highest_homes["price"].apply(self.add_commas)
+        print(highest_homes[["price", "bed", "bath", 
+                        "acre_lot", "full_address", 
+                        "house_size"]].to_string(index=False))
+
+        print("\n    Least expensive homes:\n")
+
+        lowest_homes = state_df.nsmallest(n=10, columns=["price"])
+        lowest_homes["price"] = lowest_homes["price"].apply(self.add_commas)
+        print(lowest_homes[["price", "bed", "bath", 
+                        "acre_lot", "full_address", 
+                        "house_size"]].to_string(index=False))
+
+        average_raw = round(state_df["price"].mean())
+        average = ("{:,}".format(average_raw))
+
+        print(f"\n    Average listing price in this state -- ${average}")
+
+
+    def get_by_price(self, price):
+        pass
--- main.py
+++ main.py
@@ -5,38 +5,35 @@ class Main:
 
     def __init__(self):
         self.done = False
-
         # creates a new instance of data reader, which deals with the .csv file
         self.dr = DataReader()
-        # self.draw_chart()
 
         while self.done == False:
             self.operate()
 
-        
+
     def operate(self):
 
-        user_input = input(
-            """----- What question would you like to answer? -----
-            1.) Where are the most expensive homes?
-            2.) Where are the cheapest homes?
-            3.) What is the average listing price by state?
-            4.) What is the highest listing price by state?
-            5.) What is the lowest listing price by state?
-            6.) Search by price
-            7.) quit
-            """)
+        user_input = input("""
+----- What question would you like to answer? -----
+
+    1.) Where are the most expensive homes?
+    2.) Where are the cheapest homes?
+    3.) Search by state
+    4.) Search by price
+    5.) quit
+    """)
 
         if user_input in ("1", "2"):
             self.req_homes(user_input)
 
-        elif user_input in ("3", "4", "5"):
-            self.req_by_state(user_input)
+        elif user_input == "3":
+            self.req_by_state()
 
-        elif user_input in ("6"):
-            self.req_price()
+        elif user_input == "4":
+            self.req_by_price()
 
-        elif user_input == "7":
+        elif user_input == "5":
             self.done = True
 
 
@@ -55,37 +52,29 @@ class Main:
 
 
     def req_by_state(self):
-        pass
+        all_states = self.dr.get_all_states()
+
+        print("Select a state for more information:\n")
+
+        for i in range(len(all_states)):
+            index = i + 1
+            print(f"{index}.) {all_states[i]}")
+
+        state_index = input("")
+        state = all_states[int(state_index) - 1]
+
+        self.dr.get_by_state(state)
 
 
-    def req_price(self):
-        pass
+    def req_by_price(self):
+        
+        sample = input("Enter a price between 0 and 60 million: ")
 
+        while sample < 0 or sample > 60000000:
+            sample = int(input("Please try again. "))
 
-    def draw_chart(self):
+        self.dr.get_by_price(sample)
 
-        # x-coordinates of left sides of bars 
-        left = [1, 2, 3, 4, 5]
-
-        # heights of bars
-        height = [10, 24, 36, 40, 5]
-
-        # labels for bars
-        tick_label = ['one', 'two', 'three', 'four', 'five']
-
-        # plotting a bar chart
-        plt.bar(left, height, tick_label = tick_label,
-                width = 0.8, color = ['red', 'green'])
-
-        # naming the x-axis
-        plt.xlabel('x - axis')
-        # naming the y-axis
-        plt.ylabel('y - axis')
-        # plot title
-        plt.title('My bar chart!')
-
-        # function to show the plot
-        plt.show()
 
 if __name__ == "__main__":
     Main()
