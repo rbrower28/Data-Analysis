8f1e6a71cf3015ef2aadd97f4ff4f3f21adce60a
Ryan Brower <ryan.brower28@gmail.com>
2022-06-18 09:26:32 -0700

    finished outputs and polished

--- datareader.py
+++ datareader.py
@@ -1,69 +1,92 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
 class DataReader:
 
 
     def __init__(self):
+        # Initializes the data and the test data
         self.db_name = "realtor-data.csv"
         self.test_db = "test-data.csv"
+
+        # Removes duplicate listings
         self.data = pd.read_csv(self.db_name).drop_duplicates(subset=None, keep='first', inplace=False)
         self.all_states = pd.unique(self.data["state"])
 
+
     def add_commas(self, number):
-            return ("{:,}".format(number))
+        # Turns a raw number into a comma'd one
+        # Ex: 60000000 into 60,000,000
+        return ("{:,}".format(number))
 
     
     def get_all_states(self):
+        # Returns a list of all the states
         return self.all_states
 
 
     def get_homes(self, end, quant):
+        # Gets the highest or lowest priced homes on the market
 
         if end == "high":
 
+            # Creates a new data frame with the highest prices
             new_df = self.data.nlargest(n=quant, columns=["price"])
+
+            # Adds commas to everything
             new_df["price"] = new_df["price"].apply(self.add_commas)
             new_df["house_size"] = new_df["house_size"].apply(self.add_commas)
 
+            # Prints the data
             print(new_df[["price", "bed", "bath", 
                         "acre_lot", "full_address", 
                         "house_size"]].to_string(index=False))
 
         elif end == "low":
 
+            # Creates a new data frame with the lowest prices
             new_df = self.data.nsmallest(n=quant, columns=["price"])
+
+            # Adds commas to everything
             new_df["price"] = new_df["price"].apply(self.add_commas)
             new_df["house_size"] = new_df["house_size"].apply(self.add_commas)
 
+            # Prints the data
             print(new_df[["price", "acre_lot", "full_address",
                         "house_size"]].to_string(index=False))
 
         print()
 
+        # Gets a list of the states in the data
         states_list = pd.unique(new_df["state"])
 
         total_listings = 0
 
         for state in states_list:
+            # Gets the data for each one and searches how many
+            # times it comes up in the data. It prints them out
+            # one by one
+
             state_data = new_df["state"] == state
             instances = str(state_data).count("True")
             total_listings += instances
             print(f"Listings in {state}: {instances}")
             
+        # Prints total listings
         print(f"Total listings: {total_listings}")
 
 
     def get_by_state(self, state):
+        # Gets listing information by state
         
+        # Retrieves all the homes that are in the specified state
         state_df = self.data[self.data["state"].isin([state])]
 
         print(f"\n      State -- {state}\n")
 
         print("    Most expensive homes:\n")
 
+        # Get the 10 most expensive homes and print
         highest_homes = state_df.nlargest(n=10, columns=["price"])
         highest_homes["price"] = highest_homes["price"].apply(self.add_commas)
         print(highest_homes[["price", "bed", "bath", 
@@ -72,65 +95,56 @@ class DataReader:
 
         print("\n    Least expensive homes:\n")
 
+        # Get the 10 least expensive homes and print
         lowest_homes = state_df.nsmallest(n=10, columns=["price"])
         lowest_homes["price"] = lowest_homes["price"].apply(self.add_commas)
         print(lowest_homes[["price", "bed", "bath", 
                         "acre_lot", "full_address", 
                         "house_size"]].to_string(index=False))
 
+        # Find the average listing price in the state and print
         average_raw = round(state_df["price"].mean())
         average = ("{:,}".format(average_raw))
 
         print(f"\n    Average listing price in this state -- ${average}")
 
 
-    # def p(self, low, high):
-        
-    #     data = self.data.sort_values("price")
-    #     lowest = data.nsmallest(n=1, columns=["price"])
-    #     highest = data.nlargest(n=1, columns=["price"])
-
-    #     print(price, lowest["price"].to_string(index=False), highest["price"].to_string(index=False))
-
-    #     print(float(lowest["price"].to_string(index=False)) + float(highest["price"].to_string(index=False)))
-        
-    #     closest = self.find_price(price, lowest["price"], highest["price"], data)
-
-
     def get_by_price(self, price):
+        # Get home listings around a certain price.
 
         print("\n    Slightly higher:\n")
 
+        # Creates a new data frame above the specified price
         upper_df = self.data[self.data["price"] > price]
+
+        # Takes the smallest 20 of the frame
+        # These are the 20 right above the given price.
         upper_cut = upper_df.nsmallest(n=20, columns=["price"])
+
+        # Adds commas
         upper_cut["price"] = upper_cut["price"].apply(self.add_commas)
         upper_cut["house_size"] = upper_cut["house_size"].apply(self.add_commas)
         upper_cut = upper_cut[::-1]
+
+        # Prints the data
         print(upper_cut[["price", "bed", "bath", 
                         "acre_lot", "full_address", 
                         "house_size"]].to_string(index=False))
 
         print("\n    Slightly lower\n")
 
+        # Creates a new data frame below the specified price
         lower_df = self.data[self.data["price"] < price]
+
+        # Takes the largest 20 of the frame
+        # These are the 20 right below the given price.
         lower_cut = lower_df.nlargest(n=20, columns=["price"])
+
+        # Adds commas
         lower_cut["price"] = lower_cut["price"].apply(self.add_commas)
         lower_cut["house_size"] = lower_cut["house_size"].apply(self.add_commas)
+
+        # Prints the data
         print(lower_cut[["price", "bed", "bath", 
                         "acre_lot", "full_address", 
                         "house_size"]].to_string(index=False))
-
-
-
-    # def _find_price(self, goal, min, max, data):
-
-    #     if max >= 0:
-    
-    #         middle = int((max - min) / 2) + min
-
-    #         if max - min < 2:
-    #             return min
-    #         else:
-    #             self._find_price(sorted_list, min, middle - 1, data)
-    #             self._find_price(sorted_list, middle + 1, max, data)
-
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from datareader import DataReader
-import matplotlib.pyplot as plt
 
 class Main:
 
@@ -8,11 +7,14 @@ class Main:
         # creates a new instance of data reader, which deals with the .csv file
         self.dr = DataReader()
 
+        # Starts the operation loop
         while self.done == False:
             self.operate()
 
 
     def operate(self):
+        # Runs countinually until the program is ended
+        # This happpens when the user inputs the number 5
 
         user_input = input("""
 ----- What question would you like to answer? -----
@@ -24,6 +26,7 @@ class Main:
     5.) quit
     """)
 
+        # Divides out the responses into their own methods
         if user_input in ("1", "2"):
             self.req_homes(user_input)
 
@@ -38,12 +41,16 @@ class Main:
 
 
     def req_homes(self, choice):
+        # Gets top home information
+        # Allows the user to input how many listings they want to see
 
         quant = int(input("How many listings do you want to see? (max 60): "))
 
+        # Validates that the data is between 1 and 60
         while quant < 0 or quant > 60:
             quant = int(input("Please try again. "))
 
+        # Redirects to the Data Reader file
         if choice == "1":
             self.dr.get_homes("high", quant)
 
@@ -52,10 +59,14 @@ class Main:
 
 
     def req_by_state(self):
+        # This is for the state searching method
+
+        # Returns a list of states from the csv file
         all_states = self.dr.get_all_states()
 
         print("Select a state for more information:\n")
 
+        # Displays the states with their number next to them for easy selection
         for i in range(len(all_states)):
             index = i + 1
             print(f"{index}.) {all_states[i]}")
@@ -63,16 +74,20 @@ class Main:
         state_index = input("")
         state = all_states[int(state_index) - 1]
 
+        # Redirects to the Data Reader
         self.dr.get_by_state(state)
 
 
     def req_by_price(self):
-        
+        # The method for searching by price
+
         sample = int(input("Enter a price between 0 and 60 million: "))
 
+        # validates that the number is between 0 and 60,000,000
         while sample < 0 or sample > 60000000:
             sample = int(input("Please try again. "))
 
+        # Moves over to Data Reader
         self.dr.get_by_price(sample)
 
 
